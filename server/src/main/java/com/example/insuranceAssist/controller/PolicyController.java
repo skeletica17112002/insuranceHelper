b807a1ef41c1b5c1c16b72715c6680252e600cca
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-10-05 09:33:27 +0530

    RBAC Added for Policy Controller

--- server/src/main/java/com/example/insuranceAssist/controller/PolicyController.java
+++ server/src/main/java/com/example/insuranceAssist/controller/PolicyController.java
@@ -9,6 +9,7 @@ import com.example.insuranceAssist.exception.PolicyTypeNotFoundException;
 import com.example.insuranceAssist.service.PolicyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,40 +25,48 @@ public class PolicyController {
         this.policyService = policyService;
     }
 
+    @PreAuthorize("hasRole('CLIENT')")
     @PostMapping("/create")
-    public ResponseEntity<UUID> createPolicy(@RequestBody PolicyCreateRequestDTO request) throws ClientNotFoundException, PolicyTypeNotFoundException {
+    public ResponseEntity<UUID> createPolicy(@RequestBody PolicyCreateRequestDTO request)
+            throws ClientNotFoundException, PolicyTypeNotFoundException {
         UUID policyId = policyService.createPolicy(request);
         return new ResponseEntity<>(policyId, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('CLIENT','AGENT')")
     @GetMapping("/get/{clientId}")
     public ResponseEntity<PolicyResponseDTO> getPolicy(@PathVariable UUID clientId) throws ClientNotFoundException {
         PolicyResponseDTO response = policyService.getPolicy(clientId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('CLIENT','AGENT')")
     @GetMapping("get/policyType")
     public ResponseEntity<?> getPolicyType() throws PolicyTypeNotFoundException {
         List<PolicyTypeResponseDTO> response = policyService.getPolicyType();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('CLIENT','AGENT')")
     @GetMapping("/get/policyType/{clientId}")
-    public ResponseEntity<PolicyTypeResponseDTO> getPolicyTypeByClient(@PathVariable UUID clientId) throws ClientNotFoundException {
-        PolicyTypeResponseDTO response = policyService. getPolicyTypeByClient(clientId);
+    public ResponseEntity<PolicyTypeResponseDTO> getPolicyTypeByClient(@PathVariable UUID clientId)
+            throws ClientNotFoundException {
+        PolicyTypeResponseDTO response = policyService.getPolicyTypeByClient(clientId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('AGENT')")
     @PutMapping("/update/{policyId}")
-    public ResponseEntity<?> updatePolicy(@PathVariable UUID policyId, @RequestBody PolicyCreateRequestDTO request) throws PolicyNotFoundException, PolicyTypeNotFoundException {
+    public ResponseEntity<?> updatePolicy(@PathVariable UUID policyId, @RequestBody PolicyCreateRequestDTO request)
+            throws PolicyNotFoundException, PolicyTypeNotFoundException {
         PolicyResponseDTO response = policyService.updatePolicy(policyId, request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('AGENT')")
     @DeleteMapping("/delete/{policyId}")
     public ResponseEntity<String> deletePolicy(@PathVariable UUID policyId){
         policyService.deletePolicy(policyId);
         return new ResponseEntity<>("Policy Deleted Successfully", HttpStatus.OK);
     }
-
 }
